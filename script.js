f2efa2ebfd38141c98b308d27d22f191b72c5ffc
Denis <solodkovskiy@gmail.com>
2020-01-17 16:43:12 +0300

    Скрипт перенесён в тело index.html

--- script.js
+++
@@ -1,8 +0,0 @@
-let page = document.querySelector('.page');
-let themeButton = document.querySelector('.theme-button');
-
-themeButton.onclick = function () {
-  page.classList.toggle('dark-theme');
-  themeButton('refresh');
-  page.classList.toggle('light-theme');
-};
