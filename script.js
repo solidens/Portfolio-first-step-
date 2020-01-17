e07e4c0d94cf55e16c126c85ffc685b9818199b7
Denis <solodkovskiy@gmail.com>
2020-01-17 16:41:01 +0300

    Доработана кнопка переключения цветовой схемы

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 let page = document.querySelector('.page');
 let themeButton = document.querySelector('.theme-button');
 
-themeButton.onclick = function() {
-  page.classList.toggle('light-theme');
+themeButton.onclick = function () {
   page.classList.toggle('dark-theme');
+  themeButton('refresh');
+  page.classList.toggle('light-theme');
 };
